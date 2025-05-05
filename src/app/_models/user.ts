819f2deff03b50f8a0d3d8fdd83e7d55592bb4d1
MuragProgrammer <martin.tuico@gmail.com>
2025-05-05 22:13:35 +0800

    Delete src directory

--- src/app/_models/user.ts
+++
@@ -1,9 +0,0 @@
-﻿export class User {
-    id?: string;
-    fullName?: string;
-    username?: string;
-    email?: string;
-    password?: string;
-    role?: 'admin' | 'user';
-    token?: string;
-}
